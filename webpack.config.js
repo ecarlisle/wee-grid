9d5055c6e4c67b0b0592670eb752b2febc85538e
Eric Carlisle <eric@ericcarlisle.com>
2017-11-06 16:14:16 -0500

    -Syntax correction.

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
 						{
 							loader: 'sass-loader',
 							options: {
-								outputStyle: 'expanded',
+								outputStyle: ':expanded',
 								sourceMap: false,
 							}
 						},
